1a5286bdab4b8c6c164726f75d3a9a297ae30f85
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-02 17:59:13 +0300

    Added ApplyAuditRules method to be executed before saveChanges in DbContext

--- LivesScoreUpdateSystem/LivesScoreUpdateSystem.Data.Models/User.cs
+++ LivesScoreUpdateSystem/LivesScoreUpdateSystem.Data.Models/User.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using LivesScoreUpdateSystem.Data.Models.Contracts;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace LivesScoreUpdateSystem.Data.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IDeletable, IAuditable
     {
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
--- LivesScoreUpdateSystem/LivesScoreUpdateSystem.Data/MsSqlDbContext.cs
+++ LivesScoreUpdateSystem/LivesScoreUpdateSystem.Data/MsSqlDbContext.cs
@@ -10,6 +10,26 @@ namespace LivesScoreUpdateSystem.Data
         {
         }
 
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
+
         public static MsSqlDbContext Create()
         {
             return new MsSqlDbContext();
