e503ce43407440daa783614977a460f45fd28be6
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-03 22:47:34 +0300

    Added AdminHomeController and view

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/Admin/Controllers/Abstraction/AdminController.cs
@@ -0,0 +1,10 @@
+﻿using LiveScoreUpdateSystem.Web.Infrastructure.Attributes;
+using System.Web.Mvc;
+
+namespace LiveScoreUpdateSystem.Web.Areas.Admin.Controllers.Abstraction
+{
+    [AuthorizeAdmin]
+    public abstract class AdminController : Controller
+    {
+    }
+}
---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,13 @@
+﻿using LiveScoreUpdateSystem.Web.Areas.Admin.Controllers.Abstraction;
+using System.Web.Mvc;
+
+namespace LiveScoreUpdateSystem.Web.Areas.Admin.Controllers
+{
+    public class AdminHomeController : AdminController
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
