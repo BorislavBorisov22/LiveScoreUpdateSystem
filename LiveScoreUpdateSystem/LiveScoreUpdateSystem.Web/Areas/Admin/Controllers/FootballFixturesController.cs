3de93a21a89e012490db20c17e14d955da9237cb
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-04 01:53:59 +0300

    Added football fixtures related database models

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/Admin/Controllers/FootballFixturesController.cs
@@ -0,0 +1,9 @@
+﻿using LiveScoreUpdateSystem.Web.Areas.Admin.Controllers.Abstraction;
+using System.Web.Mvc;
+
+namespace LiveScoreUpdateSystem.Web.Areas.Admin.Controllers
+{
+    public class FootballFixturesController : AdminController
+    {
+    }
+}
