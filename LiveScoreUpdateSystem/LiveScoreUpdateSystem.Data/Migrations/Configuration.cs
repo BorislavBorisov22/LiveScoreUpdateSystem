77d115fe2057ab2fe942c11ef1fbd668f3afe139
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-03 22:35:02 +0300

    Added RolesGenerating in DataBase Seed

--- LiveScoreUpdateSystem/LiveScoreUpdateSystem.Data/Migrations/Configuration.cs
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Data/Migrations/Configuration.cs
@@ -1,12 +1,19 @@
 namespace LiveScoreUpdateSystem.Data.Migrations
 {
+    using LiveScoreUpdateSystem.Common;
+    using LiveScoreUpdateSystem.Data.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<LiveScoreUpdateSystem.Data.MsSqlDbContext>
     {
-        private const string AdminRoleName = "Admin";
-        private const string LiveUpdaterName = "LiveUpdater";
-
+        private const string AdministratorUserName = "BorislavAdmin@gmail.com";
+        private const string AdministratorPassword = "123456";
+        private const string LiveUpdaterUserName = "BorislavUpdater";
+        private const string LiveUpdaterPassword = "123456";
 
         public Configuration()
         {
@@ -17,23 +24,39 @@ namespace LiveScoreUpdateSystem.Data.Migrations
 
         protected override void Seed(LiveScoreUpdateSystem.Data.MsSqlDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            this.SeedUsers(context);
         }
 
-        private void SeedUsers()
+        private void SeedUsers(MsSqlDbContext context)
         {
+            if (!context.Roles.Any())
+            {
+                CreateRole(GlobalConstants.AdminRole, context);
+                CreateRole(GlobalConstants.LiveUpdaterRole, context);
 
+                var userStore = new UserStore<User>(context);
+                var userManager = new UserManager<User>(userStore);
+                var user = new User
+                {
+                    UserName = AdministratorUserName,
+                    Email = AdministratorUserName,
+                    EmailConfirmed = true,
+                    CreatedOn = DateTime.Now
+                };
+
+                userManager.Create(user, AdministratorPassword);
+                userManager.AddToRole(user.Id, GlobalConstants.AdminRole);
+                userManager.AddToRole(user.Id, GlobalConstants.LiveUpdaterRole);
+            }
+        }
+
+        private void CreateRole(string roleName, MsSqlDbContext context)
+        {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var role = new IdentityRole { Name = roleName };
+
+            roleManager.Create(role);
         }
     }
 }
