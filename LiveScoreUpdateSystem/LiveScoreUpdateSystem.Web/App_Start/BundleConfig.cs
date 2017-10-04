4f47efd4b19d71a8f86a754eb000779a3edd7dbe
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-04 23:12:58 +0300

    Extracted modal showing js function in a separate file

--- LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/App_Start/BundleConfig.cs
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace LiveScoreUpdateSystem.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modals").Include(
+                "~/Scripts/show-modal.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Scripts/show-modal.js
@@ -0,0 +1,8 @@
+﻿function showModal(modalSelector, formTitle, action, method) {
+    $(modalSelector).modal("show")
+    $('.modal-title').html(formTitle);
+
+    $("#modal-form")
+        .attr("action", action)
+        .attr("method", method);
+}
