59dd3fd466aacbecea435db21f9dab8e978019ae
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-09 23:13:11 +0300

    Changed links in admin panel index view from magic strings to web.mvc.expression

--- LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/Admin/Controllers/FixturesController.cs
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/Admin/Controllers/FixturesController.cs
@@ -1,8 +1,14 @@
 ﻿using LiveScoreUpdateSystem.Web.Areas.Admin.Controllers.Abstraction;
+using System.Web.Mvc;
 
 namespace LiveScoreUpdateSystem.Web.Areas.Admin.Controllers
 {
     public class FixturesController : AdminController
     {
+        [HttpGet]
+        public ActionResult AddFixture()
+        {
+            return this.View();
+        }
     }
 }
