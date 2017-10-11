ec9878a9683e404ef214c200c0b8fdab2cda2c90
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-11 14:52:57 +0300

    Added base LiveUpdaterController

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/LiveUpdater/Controllers/Abstraction/LiveUpdaterController.cs
@@ -0,0 +1,10 @@
+﻿using LiveScoreUpdateSystem.Web.Controllers.Abstraction;
+using LiveScoreUpdateSystem.Web.Infrastructure.Attributes;
+
+namespace LiveScoreUpdateSystem.Web.Areas.LiveUpdater.Controllers.Abstraction
+{
+    [AuthorizeLiveUpdater]
+    public class LiveUpdaterController : BaseController
+    {
+    }
+}
