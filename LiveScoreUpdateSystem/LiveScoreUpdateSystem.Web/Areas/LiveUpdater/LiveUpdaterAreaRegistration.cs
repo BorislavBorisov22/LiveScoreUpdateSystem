f0624c1830e005e1317a4520fe817a3d6330879e
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-11 14:50:29 +0300

    Added LiveUpdaterArea

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Areas/LiveUpdater/LiveUpdaterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LiveScoreUpdateSystem.Web.Areas.LiveUpdater
+{
+    public class LiveUpdaterAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "LiveUpdater";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "LiveUpdater_default",
+                "LiveUpdater/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
