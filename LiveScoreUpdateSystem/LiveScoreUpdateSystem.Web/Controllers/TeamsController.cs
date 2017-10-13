cea3d30734e3b8fd9629a0b93f69470e1d81c00b
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-13 23:31:06 +0300

    Added teamsList view and TeamList controller method implementation

--- LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Controllers/TeamsController.cs
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Controllers/TeamsController.cs
@@ -1,8 +1,31 @@
-﻿using LiveScoreUpdateSystem.Web.Controllers.Abstraction;
+﻿using Bytes2you.Validation;
+using LiveScoreUpdateSystem.Data.Models.FootballFixtures;
+using LiveScoreUpdateSystem.Services.Data.Contracts;
+using LiveScoreUpdateSystem.Web.Controllers.Abstraction;
+using LiveScoreUpdateSystem.Web.Infrastructure.Extensions;
+using LiveScoreUpdateSystem.Web.Models;
+using System.Web.Mvc;
 
 namespace LiveScoreUpdateSystem.Web.Controllers
 {
     public class TeamsController : BaseController
     {
+        private readonly ITeamService teamService;
+
+        public TeamsController(ITeamService teamService)
+        {
+            Guard.WhenArgument(teamService, "teamService").IsNull().Throw();
+
+            this.teamService = teamService;
+        }
+
+        public ActionResult TeamsList(int page = 1, int size = 20)
+        {
+            var teams = this.teamService
+                .GetAll()
+                .Map<Team, ListTeamsViewModel>();
+
+            return this.View(teams); 
+        }
     }
 }
---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web/Models/ListTeamsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using LiveScoreUpdateSystem.Data.Models.FootballFixtures;
+using LiveScoreUpdateSystem.Web.Infrastructure.Contracts;
+
+namespace LiveScoreUpdateSystem.Web.Models
+{
+    public class ListTeamsViewModel : IMap<Team>
+    {
+        public string Name { get; set; }
+
+        public string LogoUrl { get; set; }
+    }
+}
