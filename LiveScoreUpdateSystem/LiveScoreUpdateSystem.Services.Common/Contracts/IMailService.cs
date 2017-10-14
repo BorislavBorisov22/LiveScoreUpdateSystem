70ab68145c125f8d55778087628f57ae50f34a5c
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-14 03:03:49 +0300

    Added MailService class

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Services.Common/Contracts/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LiveScoreUpdateSystem.Services.Common.Contracts
+{
+    public interface IMailService
+    {
+        void SendEmail(string subject, string content, IEnumerable<string> emails);
+    }
+}
---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Services.Common/MailService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+
+namespace LiveScoreUpdateSystem.Services.Common
+{
+    public class MailService
+    {
+        public void SendEmail(string subject, string content, IEnumerable<string> emails)
+        {
+            foreach (var email in emails)
+            {
+                var mail = new MailMessage();
+                var smtpServer = new SmtpClient();
+                smtpServer.Credentials = new NetworkCredential("BorislavAdmin@gmail.com", "123456");
+                smtpServer.Port = 587;
+                smtpServer.Host = "smtp.gmail.com";
+
+                smtpServer.EnableSsl = true;
+                smtpServer.EnableSsl = true;
+                mail.To.Add(email);
+                mail.From = new MailAddress("BorislavAdmin@gmail.com");
+                mail.Subject = subject;
+                mail.Body = content;
+                smtpServer.Send(mail);
+            }
+        }
+    }
+}
