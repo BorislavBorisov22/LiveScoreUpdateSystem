867f4aacd2c1be6143b78892f8a3b4d7070ebd5c
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-14 15:33:25 +0300

    Added globallyused image urls class

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Common/GloballyUsedPictureUrls.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveScoreUpdateSystem.Common
+{
+    public class GloballyUsedPictureUrls
+    {
+        public const string GoalScoredEvent = "~/Content/images/goal-event.png";
+        public const string RedCardEvent = "~/Content/images/red-card-event.png";
+        public const string YellowCardEvent = "~/Contenet/images/yellow-card-event.png";
+        public const string PenaltyEvent = "~/Content/images/penalty-event.png";
+    }
+}
--- LiveScoreUpdateSystem/LiveScoreUpdateSystem.Data/Migrations/201710141220072_AddedPictureUrlPropertyToFixtureEventModel.cs
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Data/Migrations/201710141220072_AddedPictureUrlPropertyToFixtureEventModel.cs
@@ -1,8 +1,7 @@
 namespace LiveScoreUpdateSystem.Data.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddedPictureUrlPropertyToFixtureEventModel : DbMigration
     {
         public override void Up()
