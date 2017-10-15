1b0c5a979601c09f9a4044de2561ee66782be3d8
Borislav Borisov <borislavborisov22@abv.bg>
2017-10-16 02:55:09 +0300

    Added SubscriptionsController unit tests

---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web.Tests/Controllers/SubscriptionsControllerTests/RemoveSubscription_Should.cs
@@ -0,0 +1,58 @@
+﻿using LiveScoreUpdateSystem.Services.Data.Contracts;
+using LiveScoreUpdateSystem.Web.Controllers;
+using LiveScoreUpdateSystem.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace LiveScoreUpdateSystem.Web.Tests.Controllers.SubscriptionsControllerTests
+{
+    [TestFixture]
+    public class RemoveSubscription_Should
+    {
+        [Test]
+        public void ReturnJsonArrayResult_WhenInvoked()
+        {
+            // arrange
+            var contextMock = new Mock<ControllerContext>();
+            contextMock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns("username");
+            contextMock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            var userService = new Mock<IUserService>();
+            var subscriptionsController = new SubscriptionsController(userService.Object);
+
+            // act
+            subscriptionsController.RemoveSubscription(null);
+
+            // assert
+            subscriptionsController.WithCallTo(c => c.RemoveSubscription(null))
+                .ShouldReturnJson();
+        }
+
+        [Test]
+        public void CallUserServiceRemoveSubscriptionMethodWithValidUsernameAndTeamName_WhenInvokedWithValidModel()
+        {
+            // arrange
+            var contextMock = new Mock<ControllerContext>();
+            contextMock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns("username");
+            contextMock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            var userService = new Mock<IUserService>();
+
+            var subscriptionsController = new SubscriptionsController(userService.Object);
+            subscriptionsController.ControllerContext = contextMock.Object;
+
+            // act
+            subscriptionsController.RemoveSubscription(new TeamSubscriptionViewModel() { Name = "TeamName"});
+
+            // assert
+            userService.Verify(u => u.RemoveSubscription("username", "TeamName"), Times.Once);
+        }
+    }
+}
---
+++ LiveScoreUpdateSystem/LiveScoreUpdateSystem.Web.Tests/Controllers/SubscriptionsControllerTests/SubscribeForTeamResults_Should.cs
@@ -0,0 +1,52 @@
+﻿using LiveScoreUpdateSystem.Services.Data.Contracts;
+using LiveScoreUpdateSystem.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace LiveScoreUpdateSystem.Web.Tests.Controllers.SubscriptionsControllerTests
+{
+    [TestFixture]
+    public class SubscribeForTeamResults_Should
+    {
+        [Test]
+        public void RedirectToIndexAction_WhenPassedTeamsNamesAreNull()
+        {
+            // arrange
+            var userService = new Mock<IUserService>();
+            var subscriptionsController = new SubscriptionsController(userService.Object);
+
+            // act 
+            subscriptionsController.SubscribeForTeamResults(null);
+
+            // assert
+            subscriptionsController.WithCallTo(c => c.SubscribeForTeamResults(null))
+                .ShouldRedirectTo(c => c.Index());
+        }
+
+        [Test]
+        public void CallUserServiceSubscribeForTeamsResultsMethod_WhenPassedTeamsNamesIsNotNull()
+        {
+            // arrange
+            var contextMock = new Mock<ControllerContext>();
+            contextMock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns("username");
+            contextMock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            var userService = new Mock<IUserService>();
+            var subscriptionsController = new SubscriptionsController(userService.Object);
+
+            var controller = new SubscriptionsController(userService.Object);
+            controller.ControllerContext = contextMock.Object;
+
+            var teams = new List<string>() { "someName"};
+
+            // act
+            controller.SubscribeForTeamResults(teams);
+
+            // assert
+            userService.Verify(u => u.SubscribeUserForTeamResults("username", teams), Times.Once);
+        }
+    }
+}
